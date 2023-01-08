02e1c912068d326563003618853154892cdb31d7
Pilar <pilar.casanueva@gmail.com>
2023-01-08 13:38:09 +0100

    mejoras

--- init-db.js
+++ init-db.js
@@ -1,36 +1,36 @@
-// Inicializar la base datos con los datos mínimos para funcionar
+// Inicializa la base datos 
 
 const readline = require('readline');
 
-// cargamos los modelos
+// carga los modelos
 const Anuncio = require('./models/Anuncio');
 
 async function main() {
 
-  // preguntar al usuario si está seguro
+  // pregunta al usuario
   const continuar = await preguntaSiNo('Estas seguro, seguro, seguro, que quieres borrar la base de datos? [n]')
   if (!continuar) {
     process.exit();
   }
 
-  // conectar a la base de datos
+  // conecta a la base de datos
   const connection = require('./lib/connectMongoose')
 
-  // inicializar la colección de anuncios
+  // inicializa la colección de anuncios
   await initAnuncio();
 
-  // desconectamos de la base de datos
+  // desconecta de la base de datos
   connection.close();
 }
 
 main().catch(err => console.log('Hubo un error', err));
 
 async function initAnuncio() {
-  // borrar todos los documentos de la colección de anuncios
+  // borra los documentos de la colección de anuncios
   const result = await Anuncio.deleteMany();
   console.log(`Eliminados ${result.deletedCount} anuncios.`);
 
-  // crear anuncios iniciales
+  // crea anuncios 
   const inserted = await Anuncio.insertMany([
     {name: "iphone", venta: "false", price: 345, photo: "foto2", tags: "mobile"},
     {name: "bicicleta", venta: "true", price: 500, photo: "foto1", tags: "motor"},
